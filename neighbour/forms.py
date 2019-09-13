f53bd96eb9db1cab666f8cc4946f51852382af0c
Brian <brayomumo5@gmail.com>
2019-09-13 18:08:28 +0300

    Create profile form

---
+++ neighbour/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import News, Business, Profile, Authorities
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+
+# class NewsForm(forms.ModelForm):
+#     class Meta:
+#         model=News
+#         exclude=['author','neighbourhood','post_date']
+
+# class UpdatebioForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude = ['username',]
+
+# class NewBusinessForm(forms.ModelForm):
+#     class Meta:
+#         model=Business
+#         exclude=['owner']
