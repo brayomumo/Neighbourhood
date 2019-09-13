212925417d96eb84aaa69fa9375b12c2eca9787d
Brian <brayomumo5@gmail.com>
2019-09-13 18:12:14 +0300

    include home url

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from neighbour import urls
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'', include(urls))
 ]
