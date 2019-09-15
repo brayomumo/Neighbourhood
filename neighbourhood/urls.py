1049571820b6f49e61a494d7ea5711f644c18de3
Brian <brayomumo5@gmail.com>
2019-09-15 20:41:44 +0300

    Add accounts urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from neighbour import urls
 from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'', include(urls))
-]
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('neighbour.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}) ,
+] 
