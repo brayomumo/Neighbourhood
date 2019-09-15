fa38d380117280c90376c3f1e6eb985eab4a3904
Brian <brayomumo5@gmail.com>
2019-09-15 20:42:25 +0300

    add urls

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -1,7 +1,22 @@
-from django.conf.urls import url,include
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.home, name="home"),
-    url(r'^accounts/', include('registration.backends.simple.urls')), 
-]
+    url(r'^$',views.home,name='home'), 
+    url(r'^create/profile$',views.create_profile, name='create_profile'),
+    url(r'^my-profile/',views.my_profile, name='my_profile'),
+    url(r'^edit/profile$', views.edit_profile, name='edit_profile'),
+    url(r'^businesses',views.businesses, name='businesses'),
+    url(r'^health',views.health, name='health'),
+    url(r'^authorities',views.authorities, name='authorities'),
+    url(r'^news',views.news, name='news'),
+    url(r'^new_news$',views.new_news, name='new_news'),
+    url(r'^search/',views.search_results, name='search_results'),
+    url(r'^new_business/',views.new_business, name='new_business'),
+]     
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
