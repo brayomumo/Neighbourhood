6e1f1f633137795f09cc2f8916abf2788141b72d
Brian <brayomumo5@gmail.com>
2019-09-15 20:33:28 +0300

    Create a health function

--- neighbour/views.py
+++ neighbour/views.py
@@ -67,3 +67,30 @@ def businesses(request):
     return render(request, 'business.html', {'business': business})
 
 
+def new_business(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+
+    if request.method == "POST":
+        form = NewBusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.owner = current_user
+            business.neighbourhood = profile.neighbourhood
+            business.save()
+
+        return HttpResponseRedirect('/businesses')
+
+    else:
+        form = NewBusinessForm()
+
+    return render(request, 'new_business.html', {"form": form})
+
+
+def health(request):
+    current_user = request.user
+    profile = Profile.objects.get(username=current_user)
+    healthservices = Health.objects.filter(
+        neighbourhood_id=profile.neighbourhood)
+
+    return render(request, 'health.html', {"healthservices": healthservices})
