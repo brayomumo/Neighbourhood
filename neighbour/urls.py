e39bcf35be818470456dd94b0b583889639f37dc
Brian <brayomumo5@gmail.com>
2019-09-15 20:59:45 +0300

    Add Search url

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -5,18 +5,17 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$',views.home,name='home'), 
-    url(r'^create/profile$',views.create_profile, name='create_profile'),
+    url(r'^$',views.home, name='home'),
+    url(r'^create/profile$', views.create_profile, name='create_profile'),
     url(r'^my-profile/',views.my_profile, name='my_profile'),
     url(r'^edit/profile$', views.edit_profile, name='edit_profile'),
-    url(r'^businesses',views.businesses, name='businesses'),
-    url(r'^health',views.health, name='health'),
-    url(r'^authorities',views.authorities, name='authorities'),
-    url(r'^news',views.news, name='news'),
-    url(r'^new_news$',views.new_news, name='new_news'),
-    url(r'^search/',views.search_results, name='search_results'),
-    url(r'^new_business/',views.new_business, name='new_business'),
-]     
-
+    url(r'^new_news$', views.new_news, name='new_news'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^new_business/', views.new_business, name='new_business'),
+    url(r'^businesses', views.businesses, name='businesses'),
+    url(r'^health', views.health, name='health'),
+    url(r'^authorities', views.authorities, name='authorities'),
+    url(r'^news', views.news, name='news'),
+]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
